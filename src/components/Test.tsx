dad8885ba40ba4bb1ce01c8d52138e6b34aebd7b
tzy <tangzeyong@xbank.plus>
2023-10-19 16:36:13 +0800

    test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -3,53 +3,59 @@ import {
   useAccount,
   useContractRead,
   useContractWrite,
+  useNetwork,
   useSendTransaction,
   useSignMessage,
   useSignTypedData,
+  useSwitchNetwork,
 } from 'wagmi'
 import { apiPostAuthChallenge, apiPostAuthLogin } from 'api/user'
 import { useState } from 'react'
 import { formatUnits, parseEther, parseUnits } from 'viem'
 import WETH_ABI from 'constants/wethAbi.json'
-// All properties on a domain are optional
-const domain = {
-  name: 'Ether Mail',
-  version: '1',
-  chainId: 1,
-  verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
-} as const
 
-// The named list of all type definitions
-const types = {
-  Person: [
-    { name: 'name', type: 'string' },
-    { name: 'wallet', type: 'address' },
-  ],
-  Mail: [
-    { name: 'from', type: 'Person' },
-    { name: 'to', type: 'Person' },
-    { name: 'contents', type: 'string' },
-  ],
-} as const
-
-const message = {
-  from: {
-    name: 'Cow',
-    wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-  },
-  to: {
-    name: 'Bob',
-    wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-  },
-  contents: 'Hello, Bob!',
-} as const
 const Test = () => {
+  const { chain } = useNetwork()
+  const { switchNetworkAsync } = useSwitchNetwork()
   const [loginMessage, setLoginMessage] = useState('')
   const { address } = useAccount()
   const { signMessageAsync } = useSignMessage()
   const [signedLoginMessage, setSignedLoginMessage] = useState('')
   const [expires, setExpires] = useState('')
   const [token, setToken] = useState('')
+
+  // All properties on a domain are optional
+  const domain = {
+    name: 'Ether Mail',
+    version: '1',
+    chainId: chain?.id,
+    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+  } as const
+
+  // The named list of all type definitions
+  const types = {
+    Person: [
+      { name: 'name', type: 'string' },
+      { name: 'wallet', type: 'address' },
+    ],
+    Mail: [
+      { name: 'from', type: 'Person' },
+      { name: 'to', type: 'Person' },
+      { name: 'contents', type: 'string' },
+    ],
+  } as const
+
+  const message = {
+    from: {
+      name: 'Cow',
+      wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+    },
+    to: {
+      name: 'Bob',
+      wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+    },
+    contents: 'Hello, Bob!',
+  } as const
   const { data, error, isError, isLoading, isSuccess, signTypedData } =
     useSignTypedData({
       domain,
@@ -63,7 +69,7 @@ const Test = () => {
     isSuccess: sendTransactionIsSuccess,
     sendTransaction,
   } = useSendTransaction({
-    to: 'tttzy.eth',
+    to: address,
     value: parseEther('0.01'),
   })
   const {
@@ -73,8 +79,10 @@ const Test = () => {
     isLoading: contractReadDataIsLoading,
     refetch,
   } = useContractRead({
-    address: process.env.REACT_APP_WETH_CONTRACT_ADDRESS as any,
-    // address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    address:
+      chain?.id === 5
+        ? process.env.REACT_APP_WETH_CONTRACT_ADDRESS
+        : ('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' as any),
     abi: WETH_ABI,
     functionName: 'balanceOf',
     args: [address],
@@ -86,8 +94,10 @@ const Test = () => {
     isSuccess: contractWriteIsSuccess,
     write,
   } = useContractWrite({
-    address: process.env.REACT_APP_WETH_CONTRACT_ADDRESS as any,
-    // address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    address:
+      chain?.id === 5
+        ? process.env.REACT_APP_WETH_CONTRACT_ADDRESS
+        : ('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' as any),
     abi: WETH_ABI,
     functionName: 'deposit',
     value: parseUnits('0.01', 18),
@@ -110,6 +120,27 @@ const Test = () => {
         <Text>{address}</Text>
       </Box>
       <Box
+        display={'flex'}
+        border={'1px solid red'}
+        padding={'10px'}
+        margin={'10px'}>
+        chainId: {chain?.id}
+      </Box>
+      {/* <Box
+        display={'flex'}
+        border={'1px solid red'}
+        padding={'10px'}
+        margin={'10px'}>
+        <Button
+          onClick={() => {
+            switchNetworkAsync?.(chain?.id).then((resp) => {
+              console.log(resp)
+            })
+          }}>
+          Switch
+        </Button>
+      </Box> */}
+      <Box
         border={'1px solid red'}
         padding={'10px'}
         margin={'10px'}>
@@ -119,7 +150,7 @@ const Test = () => {
             if (address) {
               apiPostAuthChallenge(address)
                 .then((resp) => {
-                  setLoginMessage(resp.login_message)
+                  setLoginMessage(resp?.login_message)
                 })
                 .catch((e) => {
                   console.log(e)
@@ -218,7 +249,8 @@ const Test = () => {
         </Button>
         {contractReadDataIsLoading && <div>check wallet</div>}
         <Text>
-          contract read data: {formatUnits(contractReadData as bigint, 18)}
+          contract read data:
+          {!!contractReadData && formatUnits(contractReadData as bigint, 18)}
         </Text>
         <Text>
           contract error:
