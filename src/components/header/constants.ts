e303eb7fede827eb8a1676c7e807714fba8ea477
tzy <tangzeyong@xbank.plus>
2023-10-23 11:26:20 +0800

    chore: 迁移react-scripts

---
+++ src/components/header/constants.ts
@@ -0,0 +1,29 @@
+export const BUY_NFTS_ROUTES = [
+  {
+    name: 'Market',
+    route: '/market',
+  },
+  {
+    name: 'Repay Loans',
+    route: '/loans',
+  },
+  {
+    name: 'Loan History',
+    route: '/history',
+  },
+]
+
+export const LENDING_ROUTES = [
+  {
+    name: 'Collections',
+    route: '/lending/collections',
+  },
+  {
+    name: 'My Pools',
+    route: '/lending/my-pools',
+  },
+  {
+    name: 'Loans',
+    route: '/lending/loans',
+  },
+]
