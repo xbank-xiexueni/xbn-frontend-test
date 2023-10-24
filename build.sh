293392992b15e0a0e0ed4469349eb56f6ad696a8
tzy <tangzeyong@xbank.plus>
2023-10-24 11:42:35 +0800

    fix: build flow

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-# !/bin/bash
-a=$CF_PAGES_BRANCH
-b=master
-if [ "$a" = "$b" ]
-then
-   npm run build:production
-else
-   npm i -g rimraf
-   rimraf node_modules
-   npm i --legacy-peer-deps
-   npm run build
-fi
+# # !/bin/bash
+# a=$CF_PAGES_BRANCH
+# b=master
+# if [ "$a" = "$b" ]
+# then
+#    npm run build:production
+# else
+#    npm i -g rimraf
+#    rimraf node_modules
+#    npm i --legacy-peer-deps
+#    npm run build
+# fi
